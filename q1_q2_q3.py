dfc0613512a92e3ffaed7d081e14a9a89719db72
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2022-02-04 16:46:54 +0900

    Update q1_q2_q3.py
    뇌를 자극하는 파이썬3 클래스 편을 보고 
    코드 이해가 비교적 잘됨.
    1. 매개변수가 어떤 것인지, 어떤 형태인지 파악하면 코드가 더 쉽게 이해할 수 있겠음
    2. 함수 결과 어떤 자료형, 어떤 데이터가 나오는지 확인하면 코드가 더 쉽게 이해할 수 있겠음
    3. 코드가 현재 멀티 문제(q1.a, q1.b ... )를 품고 있어서 복잡한 것 같기도 함. 앞으로 문제 만들 때 단순하게 만들기
    4. 단순하게 만들어야 선생님들이 문제 만들 수 있음.

--- q1_q2_q3.py
+++ q1_q2_q3.py
@@ -7,21 +7,21 @@ def backtickify(s):
 # a scoring mechanism that's no longer necessary
 quantum_of_bonus = 1/37
 
-def instantiate_probview(prob_cls, value_per_problem):
+def instantiate_probview(prob_cls, value_per_problem):   #problem을 인스턴스로 만든다는 함수인가? 매개변수가 첫번째는 클래스, 두번째는 ...  변수?
     # TODO: Bleh, circular import...
     from learntools.core import problem_view as pv
     from learntools.core.globals_binder import binder
     # NB: May eventually have some subclasses for this?
-    viewer_cls = pv.ProblemView
-    prob = prob_cls()
-    if prob._counts_for_points:
-        if prob._bonus:
-            prob.point_value = quantum_of_bonus
+    viewer_cls = pv.ProblemView        # problemview의 객체가 viewer_cls임
+    prob = prob_cls()                  # 받아온 클래스에서 __call__을 실행시킴.
+    if prob._counts_for_points:        # problem 모듈에서 Problem 클래스에 있는 변수
+        if prob._bonus:                # problem 모듈에서 Problem 클래스에 있는 변수
+            prob.point_value = quantum_of_bonus   # 새로운 point_value를 만듬
         else:
-            prob.point_value = value_per_problem
+            prob.point_value = value_per_problem   #아래 bind_exercise함수에서 사용됨.
     else:
         prob.point_value = 0
-    view = viewer_cls(prob, binder.readonly_globals())
+    view = viewer_cls(prob, binder.readonly_globals())  #매개변수 1번은 problem 이고 매개변수 2번은 globals 임
     # XXX: Circular reference. :/
     # Consider using weakref (https://docs.python.org/3/library/weakref.html)
     # Also, would just have preferred a cleaner separation between these abstractions...
@@ -29,7 +29,7 @@ def instantiate_probview(prob_cls, value_per_problem):
     return view
 
 
-def bind_exercises(g, exercises, start=1, var_format='q{n}'):
+def bind_exercises(g, exercises, start=1, var_format='q{n}'):                         #exercise는 문제 모듈에서 문제 클래스 변수임.
     """Given the globals() dict of an exercise module, and an ordered list of
     Problem subclasses, create a sequence of variables (by default q1, q2, q3...
     but customizable via the start and var_format kwargs) referring to instantiations
@@ -56,10 +56,10 @@ def bind_exercises(g, exercises, start=1, var_format='q{n}'):
             continue
         qno = i + start
         varname = var_format.format(n=qno)
-        assert varname not in g
+        assert varname not in g                                                            # g는 globals()로 생각됨 >> bind 모듈 확인해봐야함.
         # TODO: Probably cleaner to just pass these as sublists, rather than having
         # the MultipartProblem class start in some nascent state and get 'activated' here
-        if isinstance(prob_cls, MultipartProblem):
+        if isinstance(prob_cls, MultipartProblem):      #prob_cls가 multipartproblem 의 클래스인지 확인
             mpp = prob_cls
             g[varname] = mpp
             mpp._varname = varname
