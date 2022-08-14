c605e899ce0d81308e9001d8c1fff484fe967a77
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2022-08-14 11:38:38 +0900

    평가함수 만들기

---
+++ evaluation.py
@@ -0,0 +1,16 @@
+def test() : 
+  #입력 출력 정보 알려주기
+  _input_num = 2
+  _output_num = 1
+
+  _input = False
+  _output = False
+
+  if _input_num == len(code_input) : 
+    _input = True
+  if _output_num == len(code_output) :
+    _output= True
+  print(_input)
+  print(_output)
+
+  
